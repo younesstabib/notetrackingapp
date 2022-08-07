a54cdc2319503e294450bd71a74c6b3159998cd8
Youness <youness.t28100@gmail.com>
2022-08-07 15:52:59 +0200

    initial commit

---
+++ src/main/java/fr/tabib/notetrackingapp/NotetrackingappApplication.java
@@ -0,0 +1,13 @@
+package fr.tabib.notetrackingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotetrackingappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotetrackingappApplication.class, args);
+	}
+
+}
